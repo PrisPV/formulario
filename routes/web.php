7a543f377f1f318d2b589d42f12cae7e869dd930
Erika Pila <erikaprispi@gmail.com>
2022-01-19 21:22:10 -0500

    add-students-crud

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::resource('students', App\Http\Controllers\StudentController::class);
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
